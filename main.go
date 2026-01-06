beb1d414fde7a8562b4702380a3fd282d66c4de6
Vyacheslav Tkachuk <tkachuk@bgs.ru>
2026-01-06 19:37:00 +0300

    trig_182

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	fmt.Println("trigger_181")
+	fmt.Println("trigger_182")
 
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/about", aboutPage)
